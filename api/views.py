c24659212d6eb2887a70bbe9295c527a6a470622
Tjoseph-O <mrtemitopejoseph@gmail.com>
2025-01-29 09:58:26 +0100

    review

--- api/views.py
+++ api/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import JsonResponse
 from datetime import datetime
 from datetime import timezone
 
 def get_info(request):
+    utc_datetime = datetime.now(timezone.utc)  # Ensure UTC
+    iso_format = utc_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ") # Explicit Z
+
     data = {
         "email": "mrtemitopejoseph@gmail.com",
-        "current_datetime": datetime.now(timezone.utc).isoformat(),
+        "current_datetime": iso_format,
         "github_url": "https://github.com/Tjoseph-O/public_api_hng_stage0"
     }
     return JsonResponse(data)
